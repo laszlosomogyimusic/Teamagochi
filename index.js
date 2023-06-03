44841702b9f1cf12fdc6441d27a4f4aa0abfaf26
Laszlo Somogyi <laszlo.somogyi@gmail.com>
2023-06-03 15:37:49 +0100

    Small bugfix

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./src/page-template.js");
 
+let employees;
 
 // TODO: Write Code to gather information about the development team members, and render the HTML file.
 const questions = [
